aa02b007801bcd4a55d5d38eae19b84c51eeb25d
west0nez <102242374+west0nez@users.noreply.github.com>
2023-04-23 19:45:18 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'west0nez Kasm',
+    description: 'west0nez Kasm',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/west0nez/kasm-registrh,
+    contactUrl: 'https://github.com/west0nez/kasm-registry',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
